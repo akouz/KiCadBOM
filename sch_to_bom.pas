a8b13d8cb32d4bdb4d5ca46e0c822aa6bbe79cb4
akouz <akouz@users.noreply.github.com>
2015-06-11 20:20:49 +0930

    rev 1.02

--- sch_to_bom.pas
+++ sch_to_bom.pas
@@ -32,7 +32,6 @@ private
     Ftmp : TStringList;
     FComp: TStringList;  // current component
     FSrc : TStringList;
-    FFileNames : TStringList;
     procedure ClrComp;
     procedure AddComp;
     function  ExFootprintPrefix(val : string) : string;
@@ -41,12 +40,15 @@ private
     procedure ParseFile;
     procedure ParseString(ss:string);
 public
+    FileName : string;
     FExcludedRef : TStringList;
+    FRefList : TStringList;   // list of RefDes
     FExcludedFootPrefix : TStringList;
     FPartsList : TKicadSchPL;
     procedure MakePartList;
     procedure UpdateFields;
     procedure ReadFile(fn:string; appnd : boolean);
+    procedure SaveCSV(fn:string; BOMname: string; SG : TStringGrid);
     procedure ToStringGrid(var SG : TStringGrid);
     procedure Reset;
     constructor Create;
@@ -151,7 +153,9 @@ begin
   Result:= AnsiCompareText(s1,s2);
 end;
 
+// ##################################################
 { TKicadSchBOM }
+// ##################################################
 
 // ==================================================
 // Clear component
@@ -175,14 +179,14 @@ begin
   if Trim(FComp.Strings[0]) <> '' then begin
     s := '';
     tmp := TStringList.Create;
-    for i:=0 to ord(sepOther)-1 do begin
+    for i:=0 to ord(sepOther) do begin
       if i <= ord(sepNote) then begin
         if i = ord(sepPrice) then
           s := s + FComp.Strings[i]               // price is number
         else
           s := s + '"' + FComp.Strings[i] + '"'; // other cells are strings
-      if i < ord(sepNote) then
-        s := s + char(9);         // add TABs
+        if i < ord(sepNote) then
+          s := s + char(9);         // add TABs
       end;
       tmp.Add(FComp.Strings[i]);
     end;
@@ -224,11 +228,14 @@ begin
     ord(sepRef): begin
       if val[1] = '#' then  // power symbol
         comp_mode := false
+      else if FRefList.IndexOf(AnsiUpperCase(val)) >= 0 then // this RefDes already exists
+        comp_mode := false
       else begin
         refname := ExtractRefDesName(val,n);
         if FExcludedRef.IndexOf(AnsiUpperCase(refname)) >= 0 then
           comp_mode := false
         else begin
+          FRefList.Add(AnsiUpperCase(val));
           FComp.Strings[ord(sepRef)]  := val;
           FComp.Strings[ord(sepRefName)] := refname;
           FComp.Strings[ord(sepRefIndex)] := IntToStr(n);
@@ -263,6 +270,10 @@ begin
     ord(sepNote): begin
        FComp.Strings[ord(sepNote)] := val;
     end;
+    // ------------------------------
+    ord(sepRefUnit): begin
+       FComp.Strings[ord(sepRefUnit)] := val;
+    end;
   end;
 end;
 
@@ -270,42 +281,55 @@ end;
 // Fill component fields from parsed string (FTmp)
 // ==================================================
 procedure TKicadSchBOM.FillComp;
-var fnm : string; // field name
+var fld : string;
     fno : string;
     fval : string;
+    fnm : string; // field name
 begin
+  if FTmp.Count < 3 then
+      exit;
+  fld := FTmp.Strings[0];
+  fno := FTmp.Strings[1];
+  fval := Ftmp.Strings[2];
+  // -----------------------------------
+  // if it is a unit (part) string
+  // -----------------------------------
+  if fld = 'U' then begin
+     FillCompField(ord(sepRefUnit), fno);
   // -----------------------------------
   // if it is a filed string
   // -----------------------------------
-  if FTmp.Count >= 10 then begin
-    if FTmp.Strings[0] = 'F' then begin
-      fval := Ftmp.Strings[2];
+  end else if fld = 'F' then begin
+    if FTmp.Count < 10 then
+      exit
+    else begin
       // ---------------------------
-      // pre-defined fields
+      // pre-defined KiCad fields
       // ---------------------------
-      fno := FTmp.Strings[1];
       if fno = '0' then begin
         FillCompField(ord(sepRef), fval);
       end else if fno = '1' then begin
         FillCompField(ord(sepValue), fval);
       end  else if fno = '2' then begin
         FillCompField(ord(sepFootprint), fval);
-      end
       // ---------------------------
       // user-defined fields
       // ---------------------------
-      else if FTmp.Count > 10 then begin
-        fnm := AnsiLowerCase(FTmp.Strings[10]);
+      end else begin
+        fnm := AnsiLowerCase(FTmp.Strings[FTmp.Count-1]); // last
         if (fnm = 'brand') or (fnm = 'manuf') or (fnm = 'manufacturer') then begin
           FillCompField(ord(sepBrand), fval);
-        end else if (fnm = 'ord') or (fnm = 'order') or (fnm = 'ordering') or (fnm = 'order code')then begin
+        end else if (fnm = 'ord') or (fnm = 'order') or (fnm = 'ordering') or (fnm = 'order code')  or (fnm = 'ordering code') then begin
           FillCompField(ord(sepOrder), fval);
         end else if (fnm = 'suppl') or (fnm = 'supplier') or (fnm = 'suppliers') then begin
           FillCompField(ord(sepSupplier), fval);
-        end else if (fnm = 'price') or (fnm = 'cost') then begin
+        end else if (fnm = 'price') or (fnm = 'cost') or (fnm = '$') then begin
           FillCompField(ord(sepPrice), fval);
         end else if (fnm = 'note') or (fnm = 'notes') or (fnm = 'comment') or (fnm = 'comments') then begin
           FillCompField(ord(sepNote), fval);
+        end else if (fnm = 'inventory') or (fnm = 'inv') or (fnm = 'inv #') or (fnm = 'inv no') or
+            (fnm = '#') or (fnm = 'stock') or (fnm = 'stock no') then begin
+          FillCompField(ord(sepInv), fval);
         end;
       end;
     end;
@@ -326,14 +350,44 @@ begin
   // --------------------------
   // If file has not been loaded yet
   // --------------------------
-  if FFileNames.IndexOf(fn) < 0 then begin
-    FFileNames.Add(fn);
-    FSrc.LoadFromFile(fn);
-    ParseFile;
-    self.Sorted := false;
-    self.CustomSort(@MySort);
-    self.MakePartList;
+  FSrc.LoadFromFile(fn);
+  ParseFile;
+  self.Sorted := false;
+  self.CustomSort(@MySort);
+  self.MakePartList;
+end;
+
+// ==================================================
+// Save as CSV file
+// ==================================================
+procedure TKicadSchBOM.SaveCSV(fn: string;  BOMname: string; SG : TStringGrid);
+var cf : TStringList;
+    i : integer;
+    s : string;
+begin
+  cf := TStringList.Create;
+  // ---------------------
+  // Header block
+  // ---------------------
+  cf.Add(BOMname);         // row #1 - BOM name
+  cf.Add('');              // row #2 - spare
+  // ---------------------
+  // Field names - row #3
+  // ---------------------
+  s := '';
+  for i:=0 to SG.Columns.Count-1 do begin
+    s := s + SG.Columns[i].Title.Caption;
+    if i < SG.Columns.Count-1 then
+      s := s + char(9);
   end;
+  cf.Add(s);
+   // ---------------------
+   // Data starts from row #4
+   // ---------------------
+   for i:=0 to Count-1 do
+     cf.Add(Strings[i]);
+   cf.SaveToFile(fn);
+ cf.Free;
 end;
 
 // ==================================================
@@ -364,7 +418,7 @@ begin
         // ----------------------------
         // fill component
         // ----------------------------
-        if comp_mode and (FTmp.Strings[0] = 'F') then begin
+        if comp_mode then begin
           FillComp;
         end;
       end;
@@ -387,18 +441,24 @@ begin
   s := '';
   for i:=1 to length(ss) do begin
     c := ss[i];
-    // --------------------
+    // --------------------------
     // quoted text
-    // --------------------
+    // --------------------------
     if quote_mode then begin
       if c = char(9) then         // replace TABs
         c := ' ';
-      if was_backslash then begin // it is a second char of esc-pair
+      // ------------------
+      // it is a second char of esc-pair
+      // ------------------
+      if was_backslash then begin
         if (c = '\') or (c = '"') then
           s := s + c              // convert legal esc-pair into a single char
         else
           s := s + '\' + c;       // it should not happen: incomplete esc-pair
         was_backslash := false;   // esc-pair finished
+      // ------------------
+      // normal (eg it is not an esc-pair)
+      // ------------------
       end else begin
         if c = '"' then begin     // closing quote is a separator
           quote_mode := false;
@@ -409,9 +469,9 @@ begin
         else
           s := s + c;
       end;
-    // --------------------
+      // --------------------------
     // without quote
-    // --------------------
+    // --------------------------
     end else begin
       if c = '"' then begin
         quote_mode := true;        // quoted text started
@@ -450,8 +510,8 @@ end;
 // ==================================================
 procedure TKicadSchBOM.UpdateFields;
 var i, j : integer;
-    refnm, val, footprint, order, brand : string;
-    refnmX, valX, footprintX, orderX, brandX : string;
+    refnm, val, footprint, order, brand, inv : string;
+    refnmX, valX, footprintX, orderX, brandX, invX : string;
     sl, slX : TStringList;
 begin
   for i := 0 to Count-2 do begin
@@ -461,6 +521,7 @@ begin
     footprint  := sl.Strings[ord(sepFootprint)];
     order      := sl.Strings[ord(sepOrder)];
     brand      := sl.Strings[ord(sepBrand)];
+    inv        := sl.Strings[ord(sepInv)];
     for j := i+1 to Count-2 do begin
       slX := (self.Objects[j] as TStringList);
       refnmX      := slX.Strings[ord(sepRefName)];
@@ -468,6 +529,7 @@ begin
       footprintX  := slX.Strings[ord(sepFootprint)];
       orderX      := slX.Strings[ord(sepOrder)];
       brandX      := slX.Strings[ord(sepBrand)];
+      invX        := slX.Strings[ord(sepInv)];
       if (refnm = refnmX) and (val = valX) and (val <> 'DNP') then begin
         if (footprint = '') then begin
           sl.Strings[ord(sepFootprint)] := footprintX;
@@ -486,6 +548,8 @@ begin
             sl.Strings[ord(sepBrand)] := brandX;
           if (brandX = '') then
             slX.Strings[ord(sepBrand)] := brand;
+          if (invX = '') then
+            slX.Strings[ord(sepInv)] := inv;
         end;
       end;
     end;
@@ -498,15 +562,17 @@ end;
 procedure TKicadSchBOM.ToStringGrid(var SG: TStringGrid);
 var i, j : integer;
     sl : TStringList;
+    s : string;
 begin
   SG.RowCount := Count;
   for i:=0 to Count-1 do begin
     sl := (self.Objects[i] as TStringList);
-    for j:=0 to 7 do begin
-      if j < sl.Count then
-        SG.Cells[j,i] := sl.Strings[j]
-      else
-        SG.Cells[j,i] := '';
+    for j:=0 to ord(sepNote) do begin
+      if j < sl.Count then begin
+        s := sl.Strings[j];
+        SG.Cells[j,i] := s;
+      end else
+        SG.Cells[j,i] := '~';
     end;
   end;
 end;
@@ -525,7 +591,6 @@ begin
   Clear;
   ClrComp;
   FPartsList.Reset;
-  FFileNames.Clear;
 end;
 
 // ==================================================
@@ -538,8 +603,8 @@ begin
   FSrc := TStringList.Create;
   FTmp := TStringList.Create;
   FComp := TStringList.Create;
-  FFileNames := TStringList.Create;
   FPartsList := TKicadSchPL.Create;
+  FRefList := TStringList.Create;
   Reset;
 end;
 
@@ -552,11 +617,12 @@ begin
   FSrc.Free;
   FTmp.Free;
   FComp.Free;
-  FFileNames.Free;
   FExcludedRef.Free;
   FExcludedFootPrefix.Free;
   FPartsList.Free;
+  FRefList.Free;
   inherited Destroy;
 end;
 
 end.
+
